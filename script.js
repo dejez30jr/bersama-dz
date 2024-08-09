89f6e56d98ea51b073895f23f574b34b8f21bbce
deris <165728707+dejez30jr@users.noreply.github.com>
2024-08-09 10:23:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// const toggles = document.querySelectorAll('.faq-toggle');
+
+// toggles.forEach(toggle =>{
+//     toggle.addEventListener('click', () =>{
+//         toggle.parentNode.classList.toggle('active');
+//     })
+// })
+
+alert('boleh pinjam dulu seratus')
+
+
+const tombol = document.getElementById('tombol');
+const tombol1 = document.getElementById('tombol1');
+
+
+tombol.addEventListener('click', () => {
+    const xPos = Math.random() * (window.innerWidth - tombol.clientWidth);
+    const yPos = Math.random() * (window.innerHeight - tombol.clientHeight);
+
+    tombol.style.position = 'absolute';
+    tombol.style.left = `${xPos}px`;
+    tombol.style.top = `${yPos}px`;
+});
+tombol1.addEventListener('click', () => {
+    tombol.style.display= "none"
+   tombol1.style.display="none"
+   document.getElementById('enter').style.display= "flex";
+});
+
